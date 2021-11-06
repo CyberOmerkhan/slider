e2a9aa435a43a7135ce89449636dd58ed02f6424
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-07 02:45:04 +0600

    a v

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
 $(() => {
-    let width = $('img')[0].width;
+    let width = $('.slider img')[0].width;
     let counter = 0;
     let offset = 0;
+    let summWidth = 0;
+    let images = document.querySelectorAll('.slider img');
+    images.forEach(image => {
+        console.log(image.width);
+    })
     $('#btnLeft').bind('click', () => {
         ++clicker;
-        offset = counter;
+        offset = counter * width;
+        
     })
     $('.slider').css('overflow', 'hidden');
 })
