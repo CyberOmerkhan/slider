a9f9d2c74d6e24be70e9245b18128a1bf712915e
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-07 02:34:47 +0600

    alpha-version u know

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 $(() => {
     let width = $('img')[0].width;
+    let clicker = 0;
     $('#btnLeft').bind('click', () => {
-        
+        $('.slider img').css('left', '256px');
     })
     $('.slider').css('overflow', 'hidden');
 })
