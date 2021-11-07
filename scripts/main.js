030ead4b39aea688c06fc20baebfcc532843ede7
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-07 14:04:12 +0600

    alpha-version

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ $(() => {
     let offset = 0, counter = 0;
     $('#btnLeft').bind('click', () => {
         counter++;
-        
+        offset += images[0].width * counter;
+        console.log(offset);
+        document.querySelector('.slider_line').style.left = `${offset}px`;
     })
 })
