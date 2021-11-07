0866380a08ab7b21147744ae7756cfdd93eeaf52
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-07 14:29:16 +0600

    alpha-version

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,16 @@
 $(() => {
     const images = $('.slider img');
     $('.slider_line').css('width', `${$('.slider img').width() * images.length}px`);
-    let offset = 0, counter = 0;
+    let offset = 0, counter = 0, summWidth = 0;
+    document.querySelectorAll('.slider_line img').forEach(image => {
+        summWidth += image.width;
+    });
     $('#btnRight').bind('click', () => {
-        counter++;
-        offset -= images[0].width * counter;
+        offset -= images[0].width;
         document.querySelector('.slider_line').style.left = `${offset}px`;
-        offset = 0;
-        if(counter >= images.length - 1)
-            counter = -1;
-    })
+        if(offset < -768)
+            offset = 0;
+            document.querySelector('.slider_line').style.left = `${offset}px`;
+        console.log(offset);
+    });
 })
