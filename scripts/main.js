c8e62ca265bfe985034fe3ff0ddcb58f92229979
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-07 13:46:14 +0600

    alpha-version

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
-document.querySelectorAll('.slider img').forEach(image =>{
+const images = $('.slider img');
+$('.slider_line').css('width', `${$('.slider img').width() * images.length}px`);
+document.querySelectorAll('.slider_line img').forEach(image => {
     console.log(image.width);
 })
