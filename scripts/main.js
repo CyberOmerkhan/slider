4ca9708fe5c30289db16d8ee7b0ea4ffda6e91d4
CyberOmerkhan <amazingOmerkhan@gmail.com>
2021-11-07 14:00:23 +0600

    a-v

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
-const images = $('.slider img');
-$('.slider_line').css('width', `${$('.slider img').width() * images.length}px`);
-document.querySelectorAll('.slider_line img').forEach(image => {
-    console.log(image.width);
+$(() => {
+    const images = $('.slider img');
+    $('.slider_line').css('width', `${$('.slider img').width() * images.length}px`);
+    let offset = 0, counter = 0;
+    $('#btnLeft').bind('click', () => {
+        counter++;
+        
+    })
 })
